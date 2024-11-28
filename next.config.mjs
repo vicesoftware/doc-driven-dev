75fd667bd8076b1d8dd43e471727f5919870f77d
Ryan Vice <ryan@vicesoftware.com>
2024-11-28 11:58:00 -0600

    Add configuration for custom domains for our images in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'docdrivendevelopment.com',
+            'docdrivendev.com',
+            'documentdrivendevelopment.com',
+            'documentationdrivendevelopment.com',
+            'www.docdrivendevelopment.com',
+            'www.docdrivendev.com',
+            'www.documentdrivendevelopment.com',
+            'www.documentationdrivendevelopment.com'
+        ]
+    }
+};
 
 export default nextConfig;
